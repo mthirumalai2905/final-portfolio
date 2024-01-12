9fc3228ac21d08467f74183d70167b15f8348e84
mthirumalai2905 <mthirumalai2905@gmail.com>
2024-01-12 10:51:59 +0530

    updated with projects

--- src/App.jsx
+++ src/App.jsx
@@ -139,11 +139,30 @@ const App = () => {
     <div className='projects-container'>
      <h2 className='projects-headline'>Projects</h2>
      <div className='projects-wrapper'>
-     <a href="" className='projects-card'target='_blank'></a>
+     <div href="" className='projects-card'target='_blank'>
      <div className='projects-card-gardient'></div>
      <div className='projects-card-links'></div>
      <h3 className='projects-card-name'>Imdb Clone</h3>
+     <div className='projects-card-languages'><span>React</span><span>Firebase</span><span>API</span></div>
+     </div>
+     <div href="" className='projects-card'target='_blank'>
+     <div className='projects-card-gardient'></div>
+     <div className='projects-card-links'></div>
+     <h3 className='projects-card-name'>Sketch App</h3>
+     <div className='projects-card-languages'><span>React</span><span>Rough.js</span></div>
+     </div>
+     <div href="" className='projects-card'target='_blank'>
+     <div className='projects-card-gardient'></div>
+     <div className='projects-card-links'></div>
+     <h3 className='projects-card-name'>Blog App</h3>
+     <div className='projects-card-languages'><span>React</span><span>Mongo DB</span><span>Express</span><span>Node.js</span></div>
+     </div>
+     <div href="" className='projects-card'target='_blank'>
+     <div className='projects-card-gardient'></div>
+     <div className='projects-card-links'></div>
+     <h3 className='projects-card-name'>Project 4</h3>
      <div className='projects-card-languages'><span>React</span><span>Firebase</span></div>
+     </div>
      </div>
 
     </div>
